e9fff70698eca7e3e123674a32890db3a4a5cec6
Erdem Elvan <erdemelvan35@gmail.com>
2022-11-04 18:31:11 +0300

    Adding leaflet js library to display map with our own location

--- script.js
+++ script.js
@@ -14,9 +14,26 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const {latitude} = position.coords;
-      const {longitude} = position.coords;
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      // https://leafletjs.com/index.html --> the library
+      // Url for different themes
+      // https://leaflet-extras.github.io/leaflet-providers/preview/
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
